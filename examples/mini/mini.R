e741f24389513a93f7808f383444095597447493
jimianelli <jim.ianelli@noaa.gov>
2016-02-08 10:47:20 -0800

    added mini example

---
+++ examples/mini/mini.R
@@ -0,0 +1,11 @@
+data <- list(x=rivers)
+parameters <- list(mu=0,logSigma=0)
+
+require(TMB)
+compile('mini.cpp')
+,'-fno-gnu-unique -O0 -Wall')
+dyn.load(dynlib('mini'))
+##################
+model <- MakeADFun(data,parameters)
+fit   <- nlminb(model$par, model$fn, model$gr)
+rep   <- sdreport(model)
