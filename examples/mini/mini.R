4513aba972c5897345de78332782b3552fb68a09
Arni Magnusson <arnima@hafro.is>
2016-02-08 10:59:05 -0800

    Comment out gnu-no-unique

--- examples/mini/mini.R
+++ examples/mini/mini.R
@@ -2,8 +2,7 @@ data <- list(x=rivers)
 parameters <- list(mu=0,logSigma=0)
 
 require(TMB)
-compile('mini.cpp')
-,'-fno-gnu-unique -O0 -Wall')
+compile('mini.cpp') #,'-fno-gnu-unique -O0 -Wall')
 dyn.load(dynlib('mini'))
 ##################
 model <- MakeADFun(data,parameters)
